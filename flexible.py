8c9a799b47001b4e4c0d9e508395cc87551e99d4
Knut Tyse <knuttyse@gmail.com>
2017-01-05 16:37:15 +0200

    cleaned and separated parameters into parameters.py

---
+++ flexible.py
@@ -0,0 +1,107 @@
+import os
+import sys
+import shutil
+
+import numpy as np
+from matplotlib import mlab, cm, rcParams, colorbar, pyplot as plt
+
+from parameters import *
+from farger import plasma, viridis, inferno
+
+def last_inn_matrise(path):
+    return np.transpose(np.loadtxt(path))
+
+def snitt_matrise(matrise, xmin, xmax, ymin, ymax):
+    shape = np.shape(matrise)
+    return matrise[shape[0]-ymax:shape[0]-ymin,xmin:xmax]
+
+def get_data(path, xmin, xmax, ymin, ymax):
+    full_matrix = np.transpose(np.loadtxt(path))
+    return snitt_matrise(full_matrix, xmin, xmax, ymin, ymax)
+
+def finnminmaks(filenames, xmin, xmax, ymin, ymax):
+    minimum = 1
+    maximum = -1
+    print filenames
+    for datafile in filenames:
+        utsnitt = get_data(datafile, xmin, xmax, ymin, ymax)
+        local_min=np.min(utsnitt)
+        local_max=np.max(utsnitt)
+        if local_min < minimum:
+            minimum = local_min
+        if local_max > maximum:
+            maximum = local_max
+    return minimum, maximum
+
+datafiles =              [data_directory+dir_name+"/"+filename        for dir_name in dir_names]
+overlay_datafiles =      [data_directory+dir_name+"/"+overlay_filename for dir_name in dir_names]
+contour_line_datafiles = [data_directory+dir_name+"/"+contour_filename for dir_name in dir_names]
+
+extent = (xmin, xmax, ymin, ymax)
+minste_verdi, storste_verdi = finnminmaks(datafiles, xmin, xmax, ymin, ymax)
+norm = cm.colors.Normalize(vmin = minste_verdi, vmax = storste_verdi)
+
+if ca_number_of_contour_lines > 0:
+    minste_conturlinje, storste_conturlinje = finnminmaks(contour_line_datafiles, xmin,xmax,ymin,ymax)
+    levels = np.arange(minste_conturlinje, storste_conturlinje, (storste_conturlinje-minste_conturlinje)/ca_number_of_contour_lines)
+    rcParams['contour.negative_linestyle'] = 'solid'
+
+if not os.path.exists(save_directory):
+    os.makedirs(save_directory)
+else:
+	print "\n\n    overwrite %s? y/n" % save_directory
+	inp = raw_input()
+	if 'n' in inp:
+		sys.exit()
+
+# Plot series:
+for datafile, overlay_datafile, contour_datafile in zip(datafiles, overlay_datafiles, contour_line_datafiles) :
+    cut = get_data(datafile, xmin, xmax, ymin, ymax)
+    
+    # If overlay is desired:
+    if overlay_opacity > 0:
+        overlay = get_data(overlay_datafile, xmin, xmax, ymin, ymax)
+        cut += overlay*overlay_opacity
+
+    plt.figure()
+    plt.imshow(cut, extent=extent, cmap=cmap, norm=norm, interpolation='nearest')
+
+    # If contour lines are desired:
+    if ca_number_of_contour_lines > 0:
+        Z = get_data(contour_datafile, xmin, xmax, ymin, ymax)
+        v = plt.axis()
+        plt.contour(Z, levels, hold='on', colors='k', origin='upper', extent=extent)
+        plt.axis(v)
+
+    plt.xlabel(xlabel, fontsize = label_fontsizex)
+    plt.ylabel(ylabel, fontsize = label_fontsizey)
+    
+    if len(x_axis_ticks) > 1:
+        plt.xticks(x_axis_ticks, fontsize = label_tick_fontsizex)
+    else:
+        plt.xticks(fontsize= label_tick_fontsizex)
+    if len(y_axis_ticks) > 1:
+        plt.yticks(y_axis_ticks, fontsize=label_tick_fontsizey)
+    else:
+        plt.yticks(fontsize=label_tick_fontsizey)
+
+    if show_plots == "yes":
+        plt.show()
+    plt.savefig(save_directory + '/' + dir_name + savefile_extension, bbox_inches='tight', pad_inches=0)
+
+
+# Make Colorbar :
+if colorbar_orientation == "horizontal": # Horizontal colorbar:
+    fig = plt.figure(figsize=(8, 3))
+    ax1 = fig.add_axes([0.05, 0.80, 0.9, 0.15])
+else: # vertical colorbar:
+    fig = plt.figure(figsize=(3, 8))
+    ax1 = fig.add_axes([0.05, 0.80, 0.15, 0.9])
+
+cb1 = colorbar.ColorbarBase(ax1, cmap=cmap, norm=norm, orientation=colorbar_orientation)
+cb1.set_label(cmap_label, fontsize=colorbar_tick_fontsize)
+cb1.ax.tick_params(labelsize=colorbar_labelsize, direction='out') 
+plt.savefig(save_directory + '/colorbar' + savefile_extension, bbox_inches='tight', pad_inches=0)
+
+# Log parameters:
+shutil.copyfile("parameters.py", save_directory + "/parameters.py")
---
+++ parameters.py
@@ -0,0 +1,71 @@
+from matplotlib import  cm
+from colormaps import plasma, viridis, inferno
+
+# Parameters:
+xmin = 0
+xmax = 512
+ymin = 0
+ymax = 512
+
+# What colormap should be used?:
+cmap = cm.Reds_r # or plasma for example
+
+# Do you want a vertical or horizontal colorbar?:
+colorbar_orientation = 'vertical' #'horizontal'
+
+# What is the name of the datafiles you want to plot?:
+filename = 'j.txt'
+
+# What is the name extension you want to save your images as?:
+savefile_extension = 'J.png'
+
+# The list of the directories you want to collect data from:
+# Manually or automated by a list comprehension:
+dir_names = ['n0048', 'n0049']
+# dir_names = ['n00' + i for i in range(100)]
+
+# What is the relative path to the directory storing directories containing the datafiles?:
+data_directory = "../"
+# Where do you want to store the resulting plots?:
+save_directory = "../test/J"
+
+# Do you want an overlay with another datafile type?:
+# In that case: What is the filename of those files?:
+overlay_filename = "jc.txt"
+# How opaque do you want the overlay?:
+overlay_opacity = 0.1 # 0 means no colorbar
+
+# What should be the label of the colorbar?
+cmap_label = r"$J/J_0+0.05\tilde{J}_c$ [dimensionalless]" # label for the colorbar
+
+# What are the axes labels:
+xlabel = "$x$ [grid points]"
+ylabel = "$y$ [grid points]"
+
+# For contour lines, specify the filename of the datafiles that
+# are used for the contour lines:
+contour_filename = "g.txt"
+# If you don't want any contour lines set 0 below:
+ca_number_of_contour_lines = 8
+
+# Here you can adjust the size of the axis labels:
+# If you for some reason don't want any labels, you can set 
+# them to a very small number like 0.001
+label_fontsizex = 16
+label_fontsizey = 16
+label_tick_fontsizex = 16
+label_tick_fontsizey = 16
+
+# If you want customized axis-ticks, then specify below:
+#x_axis_ticks = [360, 380, 400, 420, 440, 460]
+# If you just want the default axis ticks, just create a
+# one element list like below:
+x_axis_ticks = [0] # length 1 means default axis ticks
+y_axis_ticks = [0]
+
+# The tick and labelsize for the colorbar:
+colorbar_tick_fontsize = 16
+colorbar_labelsize = 16
+
+# Do you want to show the plots while the program is running?
+show_plots = "no" # or "yes" if you want that
